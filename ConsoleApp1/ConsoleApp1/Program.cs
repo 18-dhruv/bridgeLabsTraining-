15801bda56187c53d822149d0dc728f0ba725e3e
18-dhruv <dhryvddogra6325@gmail.com>
2026-07-22 12:33:47 +0530

    here

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+
+namespace Pattern;
+
+public class Patter
+{
+//*
+//**
+//***
+//****
+//*****
+    static void Pattern1(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 0; j<i;j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    
+    // *****
+    // ****
+    // ***
+    // **
+    // *
+    static void patter2(int n)
+    {
+        for (int i = n; i>0; i--)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }  
+    }
+
+    static void Main(string[] args)
+    {
+        // Pattern1(3);
+    }
+}
