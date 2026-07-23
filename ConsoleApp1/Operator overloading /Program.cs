9d5057c693c224cd0eb7930f2d037e7bfe138a51
18-dhruv <dhryvddogra6325@gmail.com>
2026-07-23 15:43:15 +0530

    operator overloading

---
+++ ConsoleApp1/Operator overloading /Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace tickets
+{
+
+
+   public class Ticket
+   { 
+       int price;
+
+       public Ticket(int price)
+       {
+           this.price = price;
+       }
+
+     
+       
+
+       public static Ticket operator +(Ticket a, Ticket b)
+       {
+           return new Ticket(a.price + b.price);
+       }
+
+       public override string ToString()
+       {
+           return $"Ticket price: {price}";
+       }
+
+
+
+
+      public static void Main(string[] args)
+       {
+           Ticket a = new Ticket(100);
+           Ticket b = new Ticket(200);
+           Console.WriteLine(a+b);
+       }
+    }
+}
